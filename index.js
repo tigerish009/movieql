d831c7d2e766c9c7649d51da3db0ce9c10e09e53
tigerish009 <tigerish009@gmail.com>
2021-07-06 21:37:17 +0900

    Query & Resolvers, const server = new GraphQLServer({typeDefs,resolvers})

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
 
 const server = new GraphQLServer({
+    // 타입에 대한 정의 설정불러옴 
+    typeDefs: "graphql/schema.graphql",
+    resolvers
 
-    
 });
 
-server.start(()=>console.log("Graphql Server Running"));
+server.start(() => console.log("Graphql Server Running"));
