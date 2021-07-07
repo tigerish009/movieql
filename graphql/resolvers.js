6084781cb8a1f94d4e96f0f2163ad66b56d96e0e
tigerish009 <tigerish009@gmail.com>
2021-07-07 19:19:57 +0900

    Mutation, export const addMovie = (name, score) => {const newMovie = {id: {movies.length + 1}};...}

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,12 @@
-import { people, getById } from "./db";
+import { getMoives, getById, addMovie } from "./db";
 
 const resolvers = {
     Query: {
-        people: () => people,
-        person: (_, { id }) => getById(id)
+        movies: () => getMoives(),
+        movie: (_, { id }) => getById(id)
+    },
+    Mutation: {
+        addMovie: (_, {name, score}) => addMovie(name, score)
     }
 };
 
